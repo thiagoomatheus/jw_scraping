1a3cadcac0c88da30a2d7d76ca3498bf6326cb02
thiagoomatheus <thiagomatheus2001@hotmail.com>
2024-08-16 17:02:32 -0300

    feito página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Form from "./components/form";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex flex-col items-center justify-start gap-7 w-full">
+      <h1>Designações</h1>
+      <section className="flex flex-col w-full max-w-4xl">
+        <Form />
+      </section>
     </main>
   );
 }
