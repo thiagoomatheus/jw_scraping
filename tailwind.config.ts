1688cf5be507bdeb8930f6d3cf5914005a9fe3c2
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-02-04 17:59:17 -0300

    adicionando react-pdf e Tooltip via componente Tremor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,11 +75,37 @@ const config: Config = {
   			to: {
   				height: '0px'
   			}
-  		}
+  		},
+		hide: {
+			from: { opacity: "1" },
+			to: { opacity: "0" },
+		},
+		slideDownAndFade: {
+			from: { opacity: "0", transform: "translateY(-6px)" },
+			to: { opacity: "1", transform: "translateY(0)" },
+		},
+		slideLeftAndFade: {
+			from: { opacity: "0", transform: "translateX(6px)" },
+			to: { opacity: "1", transform: "translateX(0)" },
+		},
+		slideUpAndFade: {
+			from: { opacity: "0", transform: "translateY(6px)" },
+			to: { opacity: "1", transform: "translateY(0)" },
+		},
+			slideRightAndFade: {
+			from: { opacity: "0", transform: "translateX(-6px)" },
+			to: { opacity: "1", transform: "translateX(0)" },
+		},
+  
   	},
   	animation: {
   		accordionOpen: 'accordionOpen 150ms cubic-bezier(0.87, 0, 0.13, 1)',
-  		accordionClose: 'accordionClose 150ms cubic-bezier(0.87, 0, 0.13, 1)'
+  		accordionClose: 'accordionClose 150ms cubic-bezier(0.87, 0, 0.13, 1)',
+		hide: "hide 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+		slideDownAndFade: "slideDownAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+		slideLeftAndFade: "slideLeftAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+		slideUpAndFade: "slideUpAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+		slideRightAndFade: "slideRightAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
   	}
   },
   plugins: [require("tailwindcss-animate")],
