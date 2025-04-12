d5479e18a07b129fdcf0ac446664609411cba459
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-04-11 22:32:58 -0300

    adicionado estado para controle de notificacao e adicionado badges na ui

--- app/designacoes/componentes/cartaoDeDesignacao.tsx
+++ app/designacoes/componentes/cartaoDeDesignacao.tsx
@@ -9,6 +9,8 @@ import Btn from "@/app/minha-conta/components/btn"
 import { Contato } from "@/app/lib/types/types"
 import AccordionContatoParticipante from "./accordionContatoParticipante"
 import { editarParticipante, excluirDesignacao, notificarParticipante } from "../lib/actions/actions"
+import { Badge } from "@/app/componentes/Badge"
+import { Tooltip } from "@/app/componentes/Tooltip"
 
 type ParametrosNotificacao = {
     telefone: string
@@ -94,7 +96,7 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
     contatos: Contato[]
 } ) {
 
-    const [participante, setParticipante] = useState(designacao.participante)
+    const [dadosDesignacao, setDadosDesignacao] = useState(designacao)
     const [editar, setEditar] = useState<boolean>(false)
     const [parametros, dispatch] = useReducer(notificacoesReducer, estadoInicial)
 
@@ -103,9 +105,7 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
     const obsInputRef = useRef<HTMLInputElement>(null)
 
     return (
-        <div 
-            className={`flex flex-col gap-5 border-t-4 rounded-lg shadow-lg bg-white dark:bg-gray-800 p-6 ${designacao.parteReference.secao === "ministerio" ? "border-yellow-700" : designacao.parteReference.secao === "vida" ? "border-red-700" : designacao.parteReference.secao === "tesouros" ? "border-gray-700" : "dark:border-white border-blue-400"}`}
-        >
+        <>
             {parametros.modal === "telefone" && autorizadoParaAcoes && (
                 <Modal>
                     <h3>Notifique o participante</h3>
@@ -150,18 +150,20 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
                                     value="agora"
                                 />
                             </label>
-                            <label
-                                className="w-full flex border p-2 border-blue-300 rounded-lg flex-row gap-2 justify-between cursor-pointer"
-                            >
-                                <p>Na semana</p>
-                                <input
-                                    required
-                                    className="border border-blue-400 appearance-none rounded-full cursor-pointer checked:bg-blue-300 dark:checked:bg-blue-300 p-3"
-                                    type="radio"
-                                    name="momento_da_notificacao"
-                                    value="semana"
-                                />
-                            </label>
+                            {((!dadosDesignacao.telefone && !dadosDesignacao.notificado) || (!dadosDesignacao.telefone && dadosDesignacao.notificado)) &&
+                                <label
+                                    className="w-full flex border p-2 border-blue-300 rounded-lg flex-row gap-2 justify-between cursor-pointer"
+                                >
+                                    <p>Na semana</p>
+                                    <input
+                                        required
+                                        className="border border-blue-400 appearance-none rounded-full cursor-pointer checked:bg-blue-300 dark:checked:bg-blue-300 p-3"
+                                        type="radio"
+                                        name="momento_da_notificacao"
+                                        value="semana"
+                                    />
+                                </label>
+                            }
                         </div>
                         <p
                             className="text-[8px] lg:text-[10px]"
@@ -194,8 +196,13 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
                     <form
                         action={async () => {
                             const toastId = toast.success("Notificando ...")
-                            const resultado = await notificarParticipante(designacao.id, parametros)
+                            const resultado = await notificarParticipante(dadosDesignacao.id, parametros)
                             if (resultado.error) return toast.error(resultado.error.message, { id: toastId })
+                            setDadosDesignacao({
+                                ...dadosDesignacao,
+                                telefone: parametros.momentoDaNotificacao === "semana" ? parametros.telefone : null,
+                                notificado: parametros.momentoDaNotificacao === "agora" && true
+                            })
                             toast.success(resultado.data.message, { id: toastId })
                             dispatch({ type: "resetandoParametros" })
                         }}
@@ -307,12 +314,12 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
 
                             {parametros.formaDeTratamento === "nenhum" && !parametros.nome && <p>Olá, tudo bem?</p>}
                             {parametros.formaDeTratamento !== "nenhum" && !parametros.nome && <p>Olá {parametros.formaDeTratamento}, tudo bem?</p>}
-                            {parametros.formaDeTratamento === "nenhum" && parametros.nome && <p>Olá {designacao.participante}, tudo bem?</p>}
-                            {parametros.formaDeTratamento !== "nenhum" && parametros.nome && <p>Olá {parametros.formaDeTratamento} {designacao.participante}, tudo bem?</p>}
+                            {parametros.formaDeTratamento === "nenhum" && parametros.nome && <p>Olá {dadosDesignacao.participante}, tudo bem?</p>}
+                            {parametros.formaDeTratamento !== "nenhum" && parametros.nome && <p>Olá {parametros.formaDeTratamento} {dadosDesignacao.participante}, tudo bem?</p>}
 
-                            <p>Passando para confirmar sua designação para o dia {designacao.diaReuniao}:</p>
-                            <p className="font-bold">Designação: {designacao.parteReference.nome}</p>
-                            {parametros.tempo && <p className="font-bold">Tempo: {designacao.parteReference.tempo ? designacao.parteReference.tempo : "N/A"}</p>}
+                            <p>Passando para confirmar sua designação para o dia {dadosDesignacao.diaReuniao}:</p>
+                            <p className="font-bold">Designação: {dadosDesignacao.parteReference.nome}</p>
+                            {parametros.tempo && <p className="font-bold">Tempo: {dadosDesignacao.parteReference.tempo ? dadosDesignacao.parteReference.tempo : "N/A"}</p>}
                             {parametros.obs && parametros.obsText && <p><span className="font-bold">Obs.:</span> {parametros.obsText}</p>}
                             <p>Por favor, confirme sua participação.</p>
                             <p>Obrigado!</p>
@@ -337,81 +344,113 @@ export default function CartaoDeDesignacao( { designacao, excluir, autorizadoPar
                     </form>
                 </Modal>
             )}
-            <p>
-                <span className="font-bold">Semana:</span> {designacao.semanaReference.semana}
-            </p>
-            <p>
-                <span className="font-bold">Descrição:</span> {designacao.parteReference.nome}
-            </p>
-            <p
-                className="flex"
+            <div 
+                className={`flex flex-col gap-5 border-t-4 rounded-lg shadow-lg bg-white dark:bg-gray-800 p-6 ${dadosDesignacao.parteReference.secao === "ministerio" ? "border-yellow-700" : dadosDesignacao.parteReference.secao === "vida" ? "border-red-700" : dadosDesignacao.parteReference.secao === "tesouros" ? "border-gray-700" : "dark:border-white border-blue-400"}`}
             >
-                <span className="font-bold">Participante:</span> <Textarea className={`dark:bg-gray-800 ${editar ? "border" : "border-none"}`} value={participante} {...!editar ? { disabled: true } : { disabled: false }} onChange={(e) => setParticipante(e.target.value)} />
 
-            </p>
-            <p>
-                Tempo: {designacao.parteReference.tempo ? designacao.parteReference.tempo : "N/A"}
-            </p>
-            <p>
-                Criada por: {designacao.usuarioReference.nome}
-            </p>
-            {autorizadoParaAcoes && 
-                <div className="flex gap-5">
-                    {!editar && (
-                        <>
-                            <Btn
-                                className="bg-green-500 hover:bg-green-400"
-                                onClick={() => dispatch({ type: "iniciandoNotificacao" })}
-                            >
-                                Notificar
-                            </Btn>
-                            <Btn
-                                onClick={() => setEditar(true)}
-                            >
-                                Editar
-                            </Btn>
-                            <Btn
-                                className="bg-red-500 hover:bg-red-400"
-                                onClick={async () => {
-                                    const resultado = await excluirDesignacao(designacao.id, designacao.usuarioReference.id)
-                                    if (resultado.error) return toast.error(resultado.error.message)
-                                    excluir(designacao.id)
-                                    return toast.success(resultado.data.message)
-                                }}
-                            >
-                                Excluir
-                            </Btn>
-                        </>
-                    )}
-                    {editar && (
-                        <>
-                            <Btn
-                                className="bg-red-500 hover:bg-red-400"
-                                onClick={() => {
-                                    setParticipante(designacao.participante)
-                                    setEditar(false)
-                                }}
-                            >
-                                Cancelar
-                            </Btn>
-                            <Btn
-                                onClick={async () => {
-                                    const resultado = await editarParticipante(participante, designacao.id)
-                                    if (resultado.error) {
-                                        setEditar(false)
-                                        setParticipante(designacao.participante)
-                                        return toast.error(resultado.error.message)
-                                    }
-                                    setEditar(false)
-                                    return toast.success(resultado.data.message)
-                                }}
-                            >
-                                Salvar
-                            </Btn>
-                        </>
-                    )}
+                <div className="flex flex-row justify-between items-center">
+                    <p>
+                        <span className="font-bold">Data:</span> {dadosDesignacao.semanaReference.diaReuniao}
+                    </p>
+
+                    <div className="flex flex-row gap-2">
+                        {dadosDesignacao.telefone && (
+                            <Tooltip content="Notificação agendada na semana">
+                                <Badge className="w-fit self-end text-xs" variant="warning">Agendado</Badge>
+                            </Tooltip>
+                        )}
+                        {dadosDesignacao.notificado && (
+                            <Tooltip content="Participante notificado">
+                                <Badge className="w-fit self-end text-xs" variant="success">Notificado</Badge>
+                            </Tooltip>
+                        )}
+                    </div>
                 </div>
-            }
-        </div>
+
+                <p>
+                    <span className="font-bold">Descrição:</span> {dadosDesignacao.parteReference.nome}
+                </p>
+                <p
+                    className="flex"
+                >
+                    <span className="font-bold">Participante:</span> <Textarea className={`dark:bg-gray-800 ${editar ? "border" : "border-none"}`} value={dadosDesignacao.participante} {...!editar ? { disabled: true } : { disabled: false }} onChange={(e) => setDadosDesignacao({ ...dadosDesignacao, participante: e.target.value })} />
+
+                </p>
+                <p>
+                    Tempo: {dadosDesignacao.parteReference.tempo ? dadosDesignacao.parteReference.tempo : "N/A"}
+                </p>
+                <p>
+                    Criada por: {dadosDesignacao.usuarioReference.nome}
+                </p>
+                {autorizadoParaAcoes && 
+                    <div className="flex gap-5">
+                        {!editar && (
+                            <>
+                                <Btn
+                                    className="bg-green-500 hover:bg-green-400"
+                                    onClick={() => dispatch({ type: "iniciandoNotificacao" })}
+                                >
+                                    {dadosDesignacao.notificado ? "Reenviar" : "Notificar"}
+                                </Btn>
+
+                                {!dadosDesignacao.notificado && !dadosDesignacao.telefone && 
+                                    <Btn
+                                        onClick={() => setEditar(true)}
+                                    >
+                                        Editar
+                                    </Btn>
+                                }
+
+                                <Btn
+                                    className="bg-red-500 hover:bg-red-400"
+                                    onClick={async () => {
+                                        const resultado = await excluirDesignacao(dadosDesignacao.id, dadosDesignacao.usuarioReference.id)
+                                        if (resultado.error) return toast.error(resultado.error.message)
+                                        excluir(dadosDesignacao.id)
+                                        return toast.success(resultado.data.message)
+                                    }}
+                                >
+                                    Excluir
+                                </Btn>
+                            </>
+                        )}
+                        {editar && (
+                            <>
+                                <Btn
+                                    className="bg-red-500 hover:bg-red-400"
+                                    onClick={() => {
+                                        setDadosDesignacao({
+                                            ...dadosDesignacao,
+                                            participante: designacao.participante
+                                        })
+                                        setEditar(false)
+                                    }}
+                                >
+                                    Cancelar
+                                </Btn>
+                                <Btn
+                                    onClick={async () => {
+                                        const resultado = await editarParticipante(dadosDesignacao.participante, dadosDesignacao.id)
+                                        if (resultado.error) {
+                                            setEditar(false)
+                                            setDadosDesignacao({
+                                                ...dadosDesignacao,
+                                                participante: designacao.participante
+                                            })
+                                            return toast.error(resultado.error.message)
+                                        }
+                                        setEditar(false)
+
+                                        return toast.success(resultado.data.message)
+                                    }}
+                                >
+                                    Salvar
+                                </Btn>
+                            </>
+                        )}
+                    </div>
+                }
+            </div>
+        </>
     )
 }
