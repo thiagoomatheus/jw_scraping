63e8e504e120c46b66c02b4d41bdb74d20966551
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-05-29 10:03:09 -0300

    alterar filtragem de designações por url

--- app/designacoes/componentes/secaoDeDesignacoes.tsx
+++ app/designacoes/componentes/secaoDeDesignacoes.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { Designacao, Parte, Semana, Usuario } from "@prisma/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartaoDeDesignacao from "./cartaoDeDesignacao";
 import Btn from "@/app/minha-conta/components/btn";
 import Modal from "@/app/componentes/modal";
 import { Contato } from "@/app/lib/types/types";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function SecaoDeDesignacoes({ designacoes, autorizadoParaAcoes, contatos }: { 
     designacoes: (Designacao & { parteReference: Parte; semanaReference: Semana; usuarioReference: Usuario })[]
@@ -13,17 +14,39 @@ export default function SecaoDeDesignacoes({ designacoes, autorizadoParaAcoes, c
     contatos: Contato[]
 }) {
 
+    const searchParams = useSearchParams()
+
+    const dataFiltro = searchParams.get("data")
+    const participanteFiltro = searchParams.get("participante")
+
+    useEffect(() => {
+
+        if (!dataFiltro && !participanteFiltro) {
+            setDesignacoesFiltradas(designacoes)
+        }
+
+        if (dataFiltro && participanteFiltro) {
+            setDesignacoesFiltradas(designacoes.filter(designacao => designacao.diaReuniao === dataFiltro).filter(designacao => designacao.participante === participanteFiltro))
+        } else if (dataFiltro) {
+            setDesignacoesFiltradas(designacoes.filter(designacao => designacao.diaReuniao === dataFiltro))
+        } else if (participanteFiltro) {
+            setDesignacoesFiltradas(designacoes.filter(designacao => designacao.participante === participanteFiltro))
+        }
+    }, [dataFiltro, participanteFiltro, designacoes])
+
     const [designacoesFiltradas, setDesignacoesFiltradas] = useState<(Designacao & { parteReference: Parte; semanaReference: Semana; usuarioReference: Usuario })[]>(designacoes)
 
     const [modal, setModal] = useState<boolean>(false)
 
-    const semanas = new Set<string>(designacoes.map(designacao => designacao.semanaReference.semana))
+    const semanas = new Set<string>(designacoes.map(designacao => designacao.diaReuniao))
 
     const participantes = new Set<string>(designacoes.map(designacao => designacao.participante))
 
     function filtrarExcluidas(id:string) {
         setDesignacoesFiltradas(designacoes.filter(designacao => designacao.id !== id))
     }
+
+    const router = useRouter()
 
     return (
         <>
@@ -33,7 +56,10 @@ export default function SecaoDeDesignacoes({ designacoes, autorizadoParaAcoes, c
 
                 <Modal>
 
-                    <h3>Filtrar</h3>
+                    <div className="flex justify-between items-center w-full">
+                        <h3>Filtrar</h3>
+                        <Btn className="bg-red-700" onClick={() => setModal(false)}>X</Btn>
+                    </div>
 
                     <form
                         className="flex flex-col gap-5"
@@ -45,15 +71,15 @@ export default function SecaoDeDesignacoes({ designacoes, autorizadoParaAcoes, c
                             console.log(diaReuniao, participante);
 
                             if (!diaReuniao && !participante) {
-                                setDesignacoesFiltradas(designacoes)
+                                router.push("/designacoes")
                             }
 
                             if (diaReuniao && participante) {
-                                setDesignacoesFiltradas(designacoes.filter(designacao => designacao.semanaReference.semana === diaReuniao).filter(designacao => designacao.participante === participante))
+                                router.push(`?${new URLSearchParams({data: diaReuniao, participante: participante})}`)
                             } else if (diaReuniao) {
-                                setDesignacoesFiltradas(designacoes.filter(designacao => designacao.semanaReference.semana === diaReuniao))
+                                router.push(`?${new URLSearchParams({data: diaReuniao})}`)
                             } else if (participante) {
-                                setDesignacoesFiltradas(designacoes.filter(designacao => designacao.participante === participante))
+                                router.push(`?${new URLSearchParams({participante: participante})}`)
                             }
 
                             setModal(false)
@@ -114,7 +140,10 @@ export default function SecaoDeDesignacoes({ designacoes, autorizadoParaAcoes, c
 
                         </label>
 
-                        <Btn type="submit">Aplicar</Btn>
+                        <div className="flex justify-end items-center gap-5">
+                            <Btn type="submit">Aplicar</Btn>
+                            {(dataFiltro || participanteFiltro) && <Btn className="bg-red-700" onClick={() => router.push("/designacoes")}>Limpar</Btn>}
+                        </div>
 
                     </form>
 
